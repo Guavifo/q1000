7add7c1862096c71bb7c94e284d4b8340bb10a6d
quakkels <quakkels@gmail.com>
2017-08-15 21:48:08 -0500

    Separate nlopes/slack package from main package

--- bot/bot.go
+++ bot/bot.go
@@ -4,24 +4,108 @@ import (
 	"fmt"
 
 	"github.com/nlopes/slack"
+
+	"slogger/chatlog"
 )
 
-// Bot represents an instance of a _bot_
+// Bot represents an instance of a bot
 type Bot struct {
 	api *slack.Client
 	rtm *slack.RTM
+
+	channels map[string]string
+	users    map[string]string
 }
 
-// NewBot will make an instance of... a _new... bot_
+// NewBot will make an instance of a bot
 func NewBot(token string) (*Bot, error) {
 	if token == "" {
 		return nil, fmt.Errorf("token was empty")
 	}
 
 	api := slack.New(token)
+	rtm := api.NewRTM()
+
 	return &Bot{
 			api: api,
-			rtm: api.NewRTM(),
+			rtm: rtm,
 		},
 		nil
+}
+
+// Run the bot
+func (b *Bot) Run(log *chatlog.Log) {
+	go b.rtm.ManageConnection()
+
+	for msg := range b.rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.ConnectedEvent:
+			fmt.Println("Info: ", ev.Info)
+			fmt.Println("Connection Counter: ", ev.ConnectionCount)
+			b.rtm.SendMessage(b.rtm.NewOutgoingMessage("HI!!!.", "#general"))
+
+		case *slack.MessageEvent:
+			fmt.Printf("Message: %v\n", ev)
+			err := log.WriteLog(
+				b.getChannel(ev.Channel),
+				b.getUsername(ev.User),
+				ev.Text,
+				ev.Timestamp)
+			if err != nil {
+				fmt.Println("Error in main writeing message log: ", err)
+				recover()
+			}
+
+		case *slack.RTMError:
+			fmt.Printf("Error: %s\n", ev.Error())
+
+		case *slack.InvalidAuthEvent:
+			fmt.Printf("Invalid credentials")
+			return
+		}
+	}
+}
+func (b *Bot) setChannelList() {
+	chans, err := b.api.GetChannels(true)
+	if err != nil {
+		fmt.Println("Error when getting channels. ", err)
+		return
+	}
+
+	b.channels = make(map[string]string)
+	for _, ch := range chans {
+		b.channels[ch.ID] = ch.Name
+		fmt.Printf("id: %s, name: %s\n", ch.ID, ch.Name)
+	}
+}
+
+func (b *Bot) getChannel(id string) string {
+	if channelName, ok := b.channels[id]; ok {
+		return channelName
+	}
+
+	b.setChannelList()
+	if channelName, ok := b.channels[id]; ok {
+		return channelName
+	}
+
+	return id
+}
+
+func (b *Bot) getUsername(user string) string {
+	if username, ok := b.users[user]; ok {
+		return username
+	}
+
+	userInfo, err := b.api.GetUserInfo(user)
+	if err != nil {
+		return user
+	}
+
+	if b.users == nil {
+		b.users = make(map[string]string)
+	}
+
+	b.users[user] = userInfo.Name
+	return userInfo.Name
 }
--- slogger.go
+++ slogger.go
@@ -4,104 +4,29 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/nlopes/slack"
-
+	"slogger/bot"
 	"slogger/chatlog"
-)
-
-var (
-	api      *slack.Client
-	channels map[string]string
-	users    map[string]string
 )
 
 func main() {
 	args := os.Args[1:]
 	if len(args) != 1 {
-		panic("Token argument is missing. Cannot start.")
+		fmt.Fprintln(os.Stderr, "Token argument is missing. Cannot start.")
+		os.Exit(1)
 	}
 
 	log, err := chatlog.OpenDefault()
 	if err != nil {
-		panic("Cannot open log.")
+		fmt.Fprintln(os.Stderr, "Cannot open log.", err)
+		os.Exit(1)
 	}
 	defer log.Close()
 
-	api = slack.New(args[0])
-
-	rtm := api.NewRTM()
-	go rtm.ManageConnection()
-
-	for msg := range rtm.IncomingEvents {
-		switch ev := msg.Data.(type) {
-		case *slack.ConnectedEvent:
-			fmt.Println("Info: ", ev.Info)
-			fmt.Println("Connection Counter: ", ev.ConnectionCount)
-			rtm.SendMessage(rtm.NewOutgoingMessage("HI!!!.", "#general"))
-
-		case *slack.MessageEvent:
-			fmt.Printf("Message: %v\n", ev)
-			err := log.WriteLog(
-				getChannel(ev.Channel),
-				getUsername(ev.User),
-				ev.Text,
-				ev.Timestamp)
-			if err != nil {
-				fmt.Println("Error in main writeing message log: ", err)
-				recover()
-			}
-
-		case *slack.RTMError:
-			fmt.Printf("Error: %s\n", ev.Error())
-
-		case *slack.InvalidAuthEvent:
-			fmt.Printf("Invalid credentials")
-			return
-		}
-	}
-}
-
-func setChannelList() {
-	chans, err := api.GetChannels(true)
+	theBot, err := bot.NewBot(args[0])
 	if err != nil {
-		fmt.Println("Error when getting channels. ", err)
-		return
+		fmt.Fprintln(os.Stderr, "Error when newing a bot. ", err)
+		os.Exit(1)
 	}
 
-	channels = make(map[string]string)
-	for _, ch := range chans {
-		channels[ch.ID] = ch.Name
-		fmt.Printf("id: %s, name: %s\n", ch.ID, ch.Name)
-	}
-}
-
-func getChannel(id string) string {
-	if channelName, ok := channels[id]; ok {
-		return channelName
-	}
-
-	setChannelList()
-	if channelName, ok := channels[id]; ok {
-		return channelName
-	}
-
-	return id
-}
-
-func getUsername(user string) string {
-	if username, ok := users[user]; ok {
-		return username
-	}
-
-	userInfo, err := api.GetUserInfo(user)
-	if err != nil {
-		return user
-	}
-
-	if users == nil {
-		users = make(map[string]string)
-	}
-
-	users[user] = userInfo.Name
-	return userInfo.Name
+	theBot.Run(log)
 }
