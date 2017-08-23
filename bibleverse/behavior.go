9fd81614d28fd5b37adb599808fd7db808935797
quakkels <quakkels@gmail.com>
2017-08-23 16:31:09 -0500

    Bible verse regex can match verse ranges and also book numbers

--- bibleverse/behavior.go
+++ bibleverse/behavior.go
@@ -20,7 +20,7 @@ type Behavior struct {
 func NewBehavior() *Behavior {
 	return &Behavior{
 		books: []string{"Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"},
-		regex: regexp.MustCompile("([a-zA-Z]+)\\s(\\d+):(\\d+)"),
+		regex: regexp.MustCompile("(\\d\\s)?([a-zA-Z]+)\\s(\\d+):(\\d+)(-\\d+)?"),
 	}
 }
 
@@ -69,7 +69,7 @@ func (b *Behavior) buildMessage(verses []string) string {
 	for index, verse := range verses {
 		message += fmt.Sprintf("<https://www.biblegateway.com/passage/?search=%s|%s>", verse, verse)
 		if index < length-1 {
-			message += " - "
+			message += " • "
 		}
 	}
 
--- bibleverse/bibleverse_test.go
+++ bibleverse/bibleverse_test.go
@@ -17,22 +17,29 @@ func TestEmptyText(t *testing.T) {
 		t.Fatal("Expect 0 length result from parseText(). Got: ", len(result))
 	}
 }
+
 func TestVerseText(t *testing.T) {
 	// arrange
 	b := NewBehavior()
-	text := "Hi th1s a t3st Matthew 12:12 and this Asdf 12:12 is another Matthew 12:12 test Exodus 1:100.test"
+	text := "Hi th1s a t3st Matthew 12:12 and this Asdf 12:12 is another Matthew 12:12 test Exodus 1:100. test 1 Kings 1:1 and Exodus 1:1-10"
 
 	// act
 	result := b.parseText(text)
 
 	// assert
-	if len(result) != 2 {
-		t.Fatal("Expected length of 2 for result from parseText(). Got: ", len(result))
+	if len(result) != 4 {
+		t.Fatal("Expected length of 4 for result from parseText(). Got: ", len(result))
 	}
 	if result[0] != "Matthew 12:12" {
 		t.Fatal("Expected <Matthew 12:12> from parseText(). Got: ", result[0])
 	}
 	if result[1] != "Exodus 1:100" {
 		t.Fatal("Expected <Exodus 1:100> from parseText(). Got: ", result[1])
+	}
+	if result[2] != "1 Kings 1:1" {
+		t.Fatal("Expected <1 Kings 1:1> from parseText(). Got: ", result[1])
+	}
+	if result[3] != "Exodus 1:1-10" {
+		t.Fatal("Expected <Exodus 1:1-10> from parseText(). Got: ", result[1])
 	}
 }
